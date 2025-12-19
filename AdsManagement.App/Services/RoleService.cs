b14aeb23944f9342d1408020696cb116d9092dd6
Диана <DinDomosed@yandex.ru>
2025-12-19 16:33:24 +0300

    Feature/role service (#5)
    Реализован сервис работы с ролями (RoleService)  с unit -тестами, DTO  и AutoMapper :
    
    # Что реализовано:
      - Реализован RoleService с полным набором CRUD-операций
      - Интерфейс IRoleService, определяющий контракт сервиса и уменьшающий связанность 
      - DTO для безопасной передачи данных в/из API:
           * CreateRoleDto
           * ResponseRoleDto
           * UpdateRoleDto
       - Настроены AutoMapper-профили:
           * RoleProfile — маппинг Role <=> DTO
           * PagedResultProfile — маппинг пагинированных результатов
    
    # Тесты:
      - Добавлены unit-тесты для всех методов сервиса.
      - Проверка выброса исключений (RoleExistsException, RoleNotFoundException, ArgumentNullException)
      - Все тесты успешно проходят, используя InMemory EF Core и реальные AutoMapper профили.
    
    # Результат:
    Сервис ролей полностью реализован, покрыт тестами, корректно маппится в DTO и готов к интеграции в бизнес-логику проекта.

---
+++ AdsManagement.App/Services/RoleService.cs
@@ -0,0 +1,58 @@
+﻿using AdsManagement.App.Common;
+using AdsManagement.App.DTOs.Role;
+using AdsManagement.App.Interfaces;
+using AdsManagement.App.Interfaces.Service;
+using AdsManagement.Domain;
+using AdsManagement.Domain.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdsManagement.App.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleStorage _storage;
+        private readonly IMapper _mapper;
+        public RoleService(IRoleStorage storage, IMapper mapper)
+        {
+            _storage = storage;
+            _mapper = mapper;
+        }
+        public async Task<Guid> AddRoleAsync(CreateRoleDto roleDto, CancellationToken token = default)
+        {
+            if (roleDto == null)
+                throw new ArgumentNullException(nameof(roleDto), "The role cannot be null");
+
+            var role = _mapper.Map<Role>(roleDto);
+
+            return await _storage.AddAsync(role, token);
+        }
+        public async Task<ResponseRoleDto> GetRoleAsync(Guid id, CancellationToken token = default)
+        {
+            var dbRole = await _storage.GetAsync(id, token);
+            return _mapper.Map<ResponseRoleDto>(dbRole);
+        }
+        public async Task<PagedResult<ResponseRoleDto>> GetAllRolesAsync(int page = 1, int pageSize = 10, CancellationToken token = default)
+        {
+            var dbRoles = await _storage.GetAllAsync(page, pageSize, token);
+            return _mapper.Map<PagedResult<ResponseRoleDto>>(dbRoles);
+        }
+        public async Task DeleteRoleAsync(Guid id, CancellationToken token = default)
+        {
+            await _storage.DeleteAsync(id, token);
+        }
+        public async Task UpdateRoleAsync(UpdateRoleDto roleDto, CancellationToken token = default)
+        {
+            if(roleDto == null)
+                throw new ArgumentNullException(nameof(roleDto), "The role cannot be null");
+
+            var role = _mapper.Map<Role>(roleDto);
+            await _storage.UpdateAsync(role, token);
+        }
+    }
+}
---
+++ AdsManagement.Tests/Roles/RoleServiceTest.cs
@@ -0,0 +1,166 @@
+﻿using AdsManagement.App.DTOs.Role;
+using AdsManagement.App.Exceptions;
+using AdsManagement.App.Interfaces;
+using AdsManagement.App.Interfaces.Service;
+using AdsManagement.App.Mappings;
+using AdsManagement.App.Services;
+using AdsManagement.Data;
+using AdsManagement.Data.Storages;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace AdsManagement.Tests.Roles
+{
+    public class RoleServiceTest
+    {
+        [Fact]
+        public async Task AddRoleAsync_Test()
+        {
+            //Arrange
+            var configExpression = new MapperConfigurationExpression();
+            configExpression.AddProfile<RoleProfile>();
+            
+
+            var mapperConfig = new MapperConfiguration(configExpression, new NullLoggerFactory());
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            var options = new DbContextOptionsBuilder<AdsDbContext>()
+                .UseInMemoryDatabase("RoleServiceDbAdd")
+                .Options;
+
+            var context = new AdsDbContext(options);
+            IRoleStorage storage = new RoleStorage(context);
+            IRoleService service = new RoleService(storage, mapper);
+
+            var role1 = new CreateRoleDto() { Name = "SuperAdmin"};
+            var role2 = new CreateRoleDto() { Name = "Admin"};
+            var role3 = new CreateRoleDto() { Name = "User"};
+
+            var role1_2 = new CreateRoleDto() { Name = "SuperAdmin"};
+
+            //Act 
+            var result1 = await service.AddRoleAsync(role1);
+            var result2 = await service.AddRoleAsync(role2);
+            var result3 = await service.AddRoleAsync(role3);
+
+            //Assert + Act
+
+            await Assert.ThrowsAsync<RoleExistsException>(async () => await service.AddRoleAsync(role1_2));
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.AddRoleAsync(null));
+
+            //Assert
+            Assert.NotEqual(Guid.Empty, result1);
+            Assert.NotEqual(Guid.Empty, result2);
+            Assert.NotEqual(Guid.Empty, result3);
+        }
+        [Fact]
+        public async Task GetRoleAsync_Test()
+        {
+            //Arrange
+            var configExpression = new MapperConfigurationExpression();
+            configExpression.AddProfile<RoleProfile>();
+            configExpression.AddProfile<PagedResultProfile>();
+
+            var mapperConfig = new MapperConfiguration(configExpression, new NullLoggerFactory());
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            var options = new DbContextOptionsBuilder<AdsDbContext>()
+                .UseInMemoryDatabase("RoleServiceDbGet")
+                .Options;
+
+            var context = new AdsDbContext(options);
+            IRoleStorage storage = new RoleStorage(context);
+            IRoleService service = new RoleService(storage, mapper);
+
+            var role1 = new CreateRoleDto() { Name = "SuperAdmin" };
+            var role2 = new CreateRoleDto() { Name = "Admin" };
+            var role3 = new CreateRoleDto() { Name = "User" };
+
+            var result1 = await service.AddRoleAsync(role1);
+            var result2 = await service.AddRoleAsync(role2);
+            var result3 = await service.AddRoleAsync(role3);
+
+            //Act
+            var resultRole = await service.GetRoleAsync(result1);
+            var resultAll = await service.GetAllRolesAsync();
+
+            //Assert
+            Assert.True(resultRole is ResponseRoleDto);
+            Assert.Equal(role1.Name, resultRole.Name);
+            Assert.Equal(3, resultAll.TotalCount);
+        }
+        [Fact]
+        public async Task DeleteRoleAsync_Test()
+        {
+            //Arrange
+            var configExpression = new MapperConfigurationExpression();
+            configExpression.AddProfile<RoleProfile>();
+
+            var mapperConfig = new MapperConfiguration(configExpression, new NullLoggerFactory());
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            var options = new DbContextOptionsBuilder<AdsDbContext>()
+                .UseInMemoryDatabase("RoleServiceDbDel")
+                .Options;
+
+            var context = new AdsDbContext(options);
+            IRoleStorage storage = new RoleStorage(context);
+            IRoleService service = new RoleService(storage, mapper);
+
+            var role1 = new CreateRoleDto() { Name = "SuperAdmin" };
+            var role2 = new CreateRoleDto() { Name = "Admin" };
+
+            var result1ID = await service.AddRoleAsync(role1);
+            var result2ID = await service.AddRoleAsync(role2);
+
+            //Act
+            await service.DeleteRoleAsync(result1ID);
+            var role = await service.GetRoleAsync(result2ID);
+
+            //Assert + Act
+            await Assert.ThrowsAsync<RoleNotFoundException>(async () => await service.GetRoleAsync(result1ID));
+
+            //Assert
+            Assert.NotNull(role);
+        }
+        [Fact]
+        public async Task UpdateRoleAsync_Test()
+        {
+            //Arrange
+            var configExpression = new MapperConfigurationExpression();
+            configExpression.AddProfile<RoleProfile>();
+
+            var mapperConfig = new MapperConfiguration(configExpression, new NullLoggerFactory());
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            var options = new DbContextOptionsBuilder<AdsDbContext>()
+                .UseInMemoryDatabase("RoleServiceDbUp")
+                .Options;
+
+            var context = new AdsDbContext(options);
+            IRoleStorage storage = new RoleStorage(context);
+            IRoleService service = new RoleService(storage, mapper);
+
+            var role1 = new CreateRoleDto() { Name = "SuperAdmin" };
+            var role2 = new CreateRoleDto() { Name = "Admin" };
+
+
+            var result1ID = await service.AddRoleAsync(role1);
+            var result2ID = await service.AddRoleAsync(role2);
+
+            var roleUpDto = new UpdateRoleDto() { Id = result1ID, Name = "SuperPuperAdmin" };
+
+            //Act
+            await service.UpdateRoleAsync(roleUpDto);
+            var roleDb1 = await service.GetRoleAsync(result1ID);
+            var roleDb2 = await service.GetRoleAsync(result2ID);
+
+            //Assert
+            Assert.Equal(role2.Name, roleDb2.Name);
+            Assert.NotEqual(role1.Name, roleDb1.Name);
+            Assert.Equal(roleUpDto.Name, roleDb1.Name);
+        }
+    }
+}

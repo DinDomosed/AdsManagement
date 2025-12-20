05fb49a034b99626b2534a5f396fd4f342bb774e
Ковальчук Диана <DinDomosed@yandex.ru>
2025-12-20 21:26:55 +0300

    feat: реализован сервис управления комментариями, DTO и маппинг
    Что сделано:
    - Добавлен интерфейс ICommentService для соблюдения принципа DIP и уменьшения связанности.
    
    - Реализован CommentService с методами:
         * AddCommentAsync
         * DeleteCommentAsync
         * UpdateCommentAsync
         * GetByAdvertisementAsync
         * OnCommentEstinationChanged (для вызова события)
    
    - Добавлены DTO-модели:
         * CreateCommentDto
         * ResponceCommentDto
         * UpdateCommentDto
    
    - Добавлены AutoMapper-профили:
         * CommentProfile (маппинг Comment <=> DTO)
    
    -Событие :
        *  Реализовано событие CommentEstinationChanged, уведомляющее об изменении оценки объявления.
    
    Изменено:
        *  ICommentStorage расширен методом GetAsync (обязательное получение комментария из хранилища).
        *  В UserStorage.UpdateAsync добавлена дополнительная проверка RoleId на Guid.Empty.

--- AdsManagement.App/Interfaces/Storage/ICommentStorage.cs
+++ AdsManagement.App/Interfaces/Storage/ICommentStorage.cs
@@ -3,11 +3,8 @@ using AdsManagement.Domain.Models;
 
 namespace AdsManagement.App.Interfaces.Storage
 {
-    public interface ICommentStorage
+    public interface ICommentStorage : IStorage<Comment>
     {
-        public Task<Guid> AddAsync(Comment comment, CancellationToken token = default);
-        public Task DeleteAsync(Guid commentId, CancellationToken token = default);
-        public Task UpdateAsync(Comment comment, CancellationToken token = default);
         public Task<PagedResult<Comment>> GetByAdvertisementAsync(Guid advertisementId, int page, int pageSize, CancellationToken token = default);
     }
 }

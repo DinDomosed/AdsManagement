647fc90efae47afce46a66a7180b58639466d63b
Ковальчук Диана <DinDomosed@yandex.ru>
2026-01-14 15:40:12 +0300

    fix(migrations): исправлены типы столбцов даты на timestamptz
    - Реализована миграция для корректного хранения временных значений
    - Исправлены типы колонок для даты/времени (DateTime → timestamptz)

---
+++ AdsManagement.Data/Migrations/20260110174814_UseTimestamptzForAdAndCommentDates.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdsManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseTimestamptzForAdAndCommentDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date_of_creation",
+                table: "Comments",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Expiration_date",
+                table: "Advertisements",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date_of_creation",
+                table: "Advertisements",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date_of_creation",
+                table: "Comments",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Expiration_date",
+                table: "Advertisements",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date_of_creation",
+                table: "Advertisements",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+        }
+    }
+}

5c7110d6912cf8ffce24705d781f82cdbf8def64
Ковальчук Диана <DinDomosed@yandex.ru>
2026-01-14 13:20:12 +0300

    feat(api): добавлен адаптер FormFileDataAdapter для изоляции Application от IFormFile

---
+++ AdsManagement.API/Adapters/FormFileDataAdapter.cs
@@ -0,0 +1,17 @@
+﻿using AdsManagement.App.Interfaces.Service;
+
+namespace AdsManagement.API.Adapters
+{
+    public class FormFileDataAdapter : IFileData
+    {
+        private readonly IFormFile _formFile;
+
+        public FormFileDataAdapter(IFormFile formFile)
+        {
+            _formFile = formFile ?? throw new ArgumentNullException(nameof(formFile));
+        }
+        public string ContentType => _formFile.ContentType;
+        public long Length => _formFile.Length;
+        public Stream OpenReadStream() => _formFile.OpenReadStream();
+    }
+}
